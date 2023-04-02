9ce69c2b938a79c03b39622c16cff9d2aa25935a
ABER <1047asd44@gmail.com>
2023-04-02 23:04:16 +0900

    2023_04_02
    2023 고려대학교x연세대학교 프로그래밍 경시대회 Open Contest

---
+++ [Source_code] ABER1047/Korea_Yonsei_Univ_ProblemA.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Korea_Yonsei_Univ_ProblemA
+{
+    public static void main(String[] args)
+    {
+        char korea_str[] = {'K','O','R','E','A'};
+        char yonsei_str[] = {'Y','O','N','S','E','I'};
+        int n_founded_korea = 0;
+        int n_founded_yonsei = 0;
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String S = sc.next();
+        
+        for(int i = 0; i < S.length(); i++)
+        {
+            if (n_founded_yonsei == 6)
+            {
+                System.out.print("YONSEI");
+                break;
+            }
+            else if (n_founded_korea == 5)
+            {
+                System.out.print("KOREA");
+                break;
+            }
+        
+            char n_str = S.charAt(i);
+            //System.out.println(n_str+" "+yonsei_str[n_founded_yonsei]+" / "+n_str+" "+korea_str[n_founded_korea]);
+            n_founded_yonsei += (n_str == yonsei_str[n_founded_yonsei]) ? 1 : 0;
+            n_founded_korea += (n_str == korea_str[n_founded_korea]) ? 1 : 0;
+        }
+    }
+}
