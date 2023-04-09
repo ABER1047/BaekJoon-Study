d6306f9a834d32e9767f1cd9f88cfc51075c83e1
lhetl <hyun91000@gmail.com>
2023-04-09 23:57:23 +0900

    Create problem 1292.cpp
    1292번 "쉽게 푸는 문제"

---
+++ [Source_code] lhetl/problem 1292.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int a,b,cnt=0,i=1;
+	int arr[1000];
+	printf("%d %d %d\n",cnt,a,b);
+	while(cnt<1000){
+		for (int j=0;j<i;j++){
+			arr[cnt]=i;
+			cnt++;
+		}
+		i++;
+		printf("%d %d %d\n",cnt,a,b);
+	}
+	scanf("%d %d",&a,&b);
+	cnt=0;
+	for(int j=a;j<=b;j++){
+		cnt+=arr[j-1];
+	}
+	printf("%d",cnt);
+}
