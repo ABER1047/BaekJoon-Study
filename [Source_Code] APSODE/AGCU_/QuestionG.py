dab328c4700120947cfcebe6fb3949d0d9e72ebc
APSODE <2gunbo74@gmail.com>
2023-04-21 22:06:20 +0900

    2023 / 04 / 21 27965번 문제 / N결수 / 수학

--- [Source_Code] APSODE/AGCU_/QuestionG.py
+++ [Source_Code] APSODE/AGCU_/QuestionG.py
@@ -1,32 +1,33 @@
+import time
 from sys import stdin as si
+from math import log10, floor
 
 
 class ConsecutiveNumberString:
-    def __init__(self):
+    def __init__(self, debug_mode: bool = False):
         self._input_data = list(
             map(
                 int,
                 si.readline().rstrip("\n").split()
             )
-        )
-        self._consecutive_number = self.CreateConsecutiveNumber()
-        self._divide_target_number = self._input_data[1]
+        ) if not debug_mode else [10000000, 12341234]
         self.StartCalc()
 
-    def CreateConsecutiveNumber(self):
-        consecutive_number = ""
-        try:
-            for num in (num for num in range(self._input_data[0])):
-                consecutive_number += str(num + 1)
-
-        except StopIteration:
-            pass
-
-        return int(consecutive_number)
-
     def StartCalc(self):
-        print(self._consecutive_number % self._divide_target_number)
+        num_temp = 1
+        modulor = self._input_data[1]
+        for number in range(1, self._input_data[0]):
+            num_temp %= modulor
+            squere = floor(log10(number + 1)) + 1
+            num_temp *= 10 ** squere
+            num_temp += number + 1
+
+        print(num_temp % modulor)
 
 
 if __name__ == '__main__':
-    CNS = ConsecutiveNumberString()
+    t1 = time.perf_counter()
+    CNS = ConsecutiveNumberString(debug_mode = True)
+    t2 = time.perf_counter()
+    print(f"running time : {t2 - t1}ms")
+
