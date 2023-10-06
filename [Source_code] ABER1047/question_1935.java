d56ffa022982e4a41fc23cf0dec5a2b462b19111
ABER <1047asd44@gmail.com>
2023-10-06 13:07:15 +0900

    2023_10_06 [후위표기식2]

--- [Source_code] ABER1047/question_1935.java
+++ [Source_code] ABER1047/question_1935.java
@@ -8,73 +8,56 @@ public class question_1935
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int arg_num = Integer.parseInt(br.readLine());
-        char inputs[] = br.readLine().toCharArray();
-        String result[] = new String[inputs.length];
-        ArrayList<Float> arr = new ArrayList<>();
-        
-        int num_index = 0;
-        int operator_index = 0;
-        float parameter[] = new float[arg_num+1];
+        String inputs = br.readLine();
+        Deque<Double> dq = new LinkedList<Double>();
+
+        double parameter[] = new double[arg_num];
 
         //파라미터값 받아오기
         parameter[0] = 0;
-        for(int i = 1; i < arg_num+1; i++)
+        for(int i = 0; i < arg_num; i++)
         {
             parameter[i] = Integer.parseInt(br.readLine());
         }
         
         
-        //결과 식 세우기 (곱하기 나누기는 이때 먼저 계산)
-        for(int i = 0; i < inputs.length; i++)
+        double ans = 0;
+        for(int i = 0; i < inputs.length(); i++) 
         {
-            if (inputs[i] == '+' || inputs[i] == '-' || inputs[i] == '*' || inputs[i] == '/')
+            if(inputs.charAt(i) >= 65 && inputs.charAt(i) < 100) 
             {
-                if (inputs[i] == '*')
-                {
-                    float tmp_val = parameter[(int) result[operator_index*2-2].charAt(0)-64]*parameter[(int) result[operator_index*2].charAt(0)-64];
-                    arr.add(tmp_val);
-                    result[operator_index*2] = "@";
-                    result[operator_index*2-2] = "@";
-                    result[operator_index*2-1] = "+";
-                }
-                else if (inputs[i] == '/')
-                {
-                    float tmp_val = parameter[(int) result[operator_index*2-2].charAt(0)-64]/parameter[(int) result[operator_index*2].charAt(0)-64];
-                    arr.add(tmp_val);
-                    result[operator_index*2] = "@";
-                    result[operator_index*2-2] = "@";
-                    result[operator_index*2-1] = "+";
-                }
-                else
-                {
-                    result[operator_index*2-1] = Character.toString(inputs[i]);
-                }
-                operator_index --;
-            }
-            else
+                dq.push(parameter[inputs.charAt(i)-65]);
+            } 
+            else 
             {
-                result[num_index*2] = Character.toString(inputs[i]);
-                operator_index = num_index;
-                num_index ++;
+                if(!dq.isEmpty()) 
+                {
+                    double first = dq.pop();
+                    double second = dq.pop();
+                    char tmp_char = inputs.charAt(i);
+                    if (tmp_char == '+')
+                    {
+                        ans = second + first;
+                        dq.push(ans);
+                    }
+                    else if (tmp_char == '-')
+                    {
+                        ans = second-first;
+                        dq.push(ans);
+                    }
+                    else if (tmp_char == '*')
+                    {
+                        ans = second*first;
+                        dq.push(ans);
+                    }
+                    else
+                    {
+                        ans = second/first;
+                        dq.push(ans);
+                    }
+                }
             }
         }
-        
-        //식 계산
-        float ans = parameter[(int) result[0].charAt(0)-64];
-        for(int i = 1; i < inputs.length-1; i += 2)
-        {
-            System.out.print(result[i-1]+" "+result[i]+" ");
-            if (result[i].charAt(0) == '+')
-            {
-                ans += parameter[(int) result[i+1].charAt(0)-64];
-            }
-            else
-            {
-                ans -= parameter[(int) result[i+1].charAt(0)-64];
-            }
-        }
-        
-        System.out.print(arr);
-        System.out.printf("%.2f",ans);
+        System.out.printf("%.2f",+ans);
     }
 }
