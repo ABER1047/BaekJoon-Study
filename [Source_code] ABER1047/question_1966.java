199bf69cff52441bdf841f0c5b3047a1f14996ef
ABER <1047asd44@gmail.com>
2023-10-17 14:34:26 +0900

    2023_10_17 [프린트 큐]

--- [Source_code] ABER1047/question_1966.java
+++ [Source_code] ABER1047/question_1966.java
@@ -8,52 +8,72 @@ public class question_1966
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));        
         
-        //arraylist 생성
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        
-        
+
         //테스트 케이스
         int test_case = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < test_case; i++)
         {
+            //arraylist 생성
+            ArrayList<Integer> arr = new ArrayList<>();
+            ArrayList<Integer> arr_highest = new ArrayList<>();
+            
             int ans = 0;
             String str[] = br.readLine().split(" ");
             int doc_num = Integer.parseInt(str[0]);
             int n_doc_pos = Integer.parseInt(str[1]);
             
-            int highest_imp_ind = 0;
-            
             //문서 인풋값
             String str_doc_input[] = br.readLine().split(" ");
             for(int ii = 0; ii < doc_num; ii++)
             {
-                arr.add(Integer.parseInt(str_doc_input[ii]));
+                int tmp_val = Integer.parseInt(str_doc_input[ii]);
+                arr.add(tmp_val);
+                arr_highest.add(tmp_val);
             }
             
-
+            //중요도 높은 순서대로 정렬
+            Collections.sort(arr_highest,Collections.reverseOrder());
+            
             //전체 문서중에 더 중요도가 높은게 있는지 확인
-            for(int ii = 1; ii < arr.size(); ii++)
+            int highest_num = arr_highest.remove(0);
+            while(true)
             {
-                if (arr.get(0) < arr.get(ii))
+                int n_num = arr.remove(0);
+                
+                //우선도 체크
+                if (n_num >= highest_num)
                 {
-                    highest_imp_ind = ii;
+                    ans ++;
+                    if (highest_num == n_num)
+                    {
+                        // bw.write((arr)+"\t\t"+(n_doc_pos)+" / "+(highest_num)+"\n");
+                        if (arr_highest.isEmpty())
+                        {
+                            break;
+                        }
+                        highest_num = arr_highest.remove(0);
+                        
+                        if (n_doc_pos <= 0)
+                        {
+                            break;
+                        }
+                    }
                 }
+                else //숫자 뒤로 넘기기
+                {
+                    arr.add(n_num);
+                }
+                n_doc_pos --;
+                
+                if (n_doc_pos < 0)
+                {
+                    n_doc_pos = arr.size()-1;
+                }
+                // bw.write((arr)+"\t"+(n_doc_pos)+" / "+(highest_num)+"\n");
             }
-            
-            if (highest_imp_ind == 0)
-            {
-                arr.get(highest_imp_ind);
-                arr.remove(highest_imp_ind);
-            }
-            else
-            {
-                ans++;
-            }
-            
-            arr.get(highest_imp_ind);
-            arr.remove(highest_imp_ind);
+
+            bw.write(ans+"\n");
         }
 
         
